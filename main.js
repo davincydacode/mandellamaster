fb1e4fbd768bf4144d803df009c1fb3cff74fcaf
unknown <hademylola@gmail.com>
2018-03-19 11:36:36 -0700

    first Commikt

---
+++ main.js
@@ -0,0 +1,13 @@
+function show(){
+    alert("Me");
+}
+function confirm(status){
+
+    if(status){
+        x = confirm("Do wanna save status?")
+
+        if(x){
+            alert('Satus Succesfully saved');
+        }
+    }
+}
